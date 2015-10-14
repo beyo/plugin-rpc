75144036eb95be366bcf2d6ac98f10114be24af8
yanickrochon <yanick.rochon@gmail.com>
2015-10-14 17:42:28 -0400

    Client library served!

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 
+const LIBRARY_PATTERN_TOKENS = /\/\*+\{\{(.+?)\}\}\*+\/(.*?)\/\*+\{\{!\1\}\}\*+\//g;
+
+var path = require('path');
 var crypto = require("crypto")
 
 var browserify = require('browserify');
 var esmangleify = require('esmangleify');
+var browserifyReplace = require('browserify-replace');
+var esprima = require('esmangleify/node_modules/browserify-esprima-tools/node_modules/esprima/esprima');
+var escodegen = require('esmangleify/node_modules/browserify-esprima-tools/node_modules/escodegen/escodegen');
+var esmangle = require('esmangleify/node_modules/esmangle/lib/esmangle');
 
 var RPCServer = require('./lib/rpc-server');
 
 var rpc;
+var libraryTemplate;
 
 
 module.exports = rpcPlugin;
@@ -17,6 +25,8 @@ function * rpcPlugin(beyo, options) {
     return rpc;
   }
 
+  options = options || {};
+
   rpc = new RPCServer(options);
   rpc.middleware = middlewareWrapper(beyo, options.clientOptions);
 
@@ -24,7 +34,11 @@ function * rpcPlugin(beyo, options) {
 }
 
 function middlewareWrapper(beyo, config) {
-  var ownerProvider = config.ownerProvider;
+  var ownerProvider;
+
+  config = config || {};
+
+  ownerProvider = config.ownerProvider;
 
   if (typeof ownerProvider === 'string') {
     ownerProvider = propertyOwnerProvider(ownerProvider);
@@ -39,7 +53,7 @@ function middlewareWrapper(beyo, config) {
       if (config.url && (this.url === config.url)) {
         id = yield ownerProvider(this);
 
-        this.body = yield* clientLibrary(id, this);
+        this.body = yield clientLibrary(id, config);
       } else {
         yield* next;
       }
@@ -60,7 +74,7 @@ function propertyOwnerProvider(property) {
       if (ctx !== undefined) {
         cb(null, ctx);
       } else {
-        cb("anonymous");
+        cb(null, "anonymous");
       }
     };
   };
@@ -69,22 +83,94 @@ function propertyOwnerProvider(property) {
 
 function defaultOwnerProvider() {
   return function (cb) {
-    crypto.randomBytes(256, cb);
+    crypto.randomBytes(64, function (err, buf) {
+      cb(err, buf.toString('base64'));
+    });
   };
 }
 
 
-function * clientLibrary(id, ctx) {
-  var b = browserify();
-  var buffer = '';
-
+function clientLibrary(id, options) {
   return function (done) {
+    if (libraryTemplate) {
+      done(null, configClientLibrary(id, options));
+    } else {
+      var tokens = {
+        'owner': id,
+        'primus-url': options.url
+      };
+      var b = browserify({
+        basedir: path.join(__dirname, 'lib'),
+        standalone: 'beyo.plugins.rpc'
+      });
+      var buffer = getPrimusLibrary(false) + '\n// ============= RPC =============\n';
 
-    b.add('./rpc-client');
-    b.bundle().on('data', function (data) {
-      buffer = buffer + data.toString();
-    }).on('end', function () {
-      done(null, buffer);
-    });
+      b.transform(browserifyReplace, {
+        'replace': [{
+          'from': LIBRARY_PATTERN_TOKENS,
+          'to': function (match, token, old) {
+            return 'beyo.plugins.rpc.config["' + token + '"]';
+          }
+        }]
+      });
+      //b.transform(esmangleify());
+      b.add('./rpc-client');
+
+      b.bundle().on('data', function (data) {
+        buffer = buffer + data.toString();
+      }).on('end', function () {
+        libraryTemplate = buffer;
+
+        done(null, configClientLibrary(id, options));
+      });
+    }
   };
+}
+
+
+function getPrimusLibrary(optimize) {
+  var buffer = rpc.primus.library();
+
+  if (optimize) {
+    var ast = esprima.parse(buffer);
+    // Get optimized AST
+    var optimized = esmangle.optimize(ast, null);
+    // gets mangled AST
+    var result = esmangle.mangle(optimized);
+    // get source back
+    buffer = escodegen.generate(result, {
+      format: {
+        renumber: true,
+        hexadecimal: true,
+        escapeless: true,
+        compact: true,
+        semicolons: false,
+        parentheses: false
+      }
+    });
+  }
+  
+  return '\n// ============= Primus =============\n' 
+       + '!function(module,exports,define){'
+       + buffer
+      + '}();'
+  ;
+}
+
+
+function configClientLibrary(id, options) {
+  var pre = '\n// ============= Config =============\n'
+          //+ 'var exports,module,define;'
+          + 'var beyo=window.beyo||{};'
+          + 'beyo.plugins=beyo.plugins||{};'
+          + 'beyo.plugins=beyo.plugins||{};'
+          + 'beyo.plugins.rpc=beyo.plugins.rpc||{};'
+          + 'beyo.plugins.rpc.config=' + JSON.stringify({
+              'owner': id,
+              'primus-url': rpc.clientConnection.url,
+              'primus-options': options.primusOptions || {}
+            }) + ';'
+  ;
+
+  return '!function(window){' + pre + libraryTemplate + '}(this||window||{})';
 }
